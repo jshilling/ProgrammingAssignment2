0f180da6d8f62ed1e14ea1b79b111eefe5589df5
Jeff Shilling <jeff@shilling.us.com>
2014-08-17 22:58:48 -0400

    Initial comments on stub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Programm Assignment 2 for "R Programming" (rprog-006)
+##
+## The functions cache tine inverse of a square, invertable matric
+## so that it does not have to be re-calculated for every use (unless
+## the matrix, and therefore it's inverse, have changed).
+##
+## Jeff Shilling
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## This function creates a special "matrix" object
+    ## that can cache its inverse.
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## If the inverse has already been calculated 
+        ## (and the matrix has not changed), then
+        ## `cacheSolve` retrieves the inverse from the cache.
+    
 }
